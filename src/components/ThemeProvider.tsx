5d220a116a222c5aae7c1af5c7cec5532d630094
Kyle <knewsam@gmail.com>
2025-03-17 17:07:08 +0800

    fix: resolve build errors - update ESLint dependencies and fix next-themes import

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
